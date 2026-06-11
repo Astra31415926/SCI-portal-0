d050f2f3fa69b56dc8b7e5f56f5f10070ab733b1
Mihail Kashkarov <bakminsterfuler@gmail.com>
2026-06-11 22:48:16 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react' // или другой ваш плагин, если он там был прописан
 
 export default defineConfig({
-  plugins: [react()],
   base: '/SCI-portal-0/',
+  server: {
+    port: 3000
+  }
 })
